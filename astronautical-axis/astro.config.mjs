0598df5ebe2406ea50ffa5db7332678006bd0d53
GratzStefan <stefan.gratz2@gmail.com>
2024-11-15 09:55:20 +0100

    Added dependencies

--- astronautical-axis/astro.config.mjs
+++ astronautical-axis/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static', // Ensures Astro generates static files
-    base: '/'
-  });
+  base: '/',
+  adapter: netlify()
+});
