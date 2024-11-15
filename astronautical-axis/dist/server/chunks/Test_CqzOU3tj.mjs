cfd1231149bc50f5a6e1ed53ee8a5be420caa160
GratzStefan <stefan.gratz2@gmail.com>
2024-11-15 10:32:27 +0100

    Template

---
+++ astronautical-axis/dist/server/chunks/Test_CqzOU3tj.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./Test_rqLR4ge_.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
