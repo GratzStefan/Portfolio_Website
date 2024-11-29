32649e6644b82f82a976dc7f5d7f2db111f931bf
GratzStefan <stefan.gratz2@gmail.com>
2024-11-29 10:15:00 +0100

    Ran "npm run build"

---
+++ astronautical-axis/.netlify/v1/functions/ssr/.netlify/build/chunks/Test 2_B1ilVqPy.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./Test 2_CTqRny4o.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
